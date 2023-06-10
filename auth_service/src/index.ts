47e8b781bc59ec20f5cb5ff3096d6e3d793ae061
Valentina Restrepo <73320777+ValentinaRestrepo2@users.noreply.github.com>
2023-06-10 07:14:36 -0500

    Correcciones docker compose #4

---
+++ auth_service/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, json } from 'express'
+import cors from 'cors'
+const server = express();
+
+server.use(json())
+server.use(cors())
+
+const users: { [key: string  | number ]: any } = {
+  "747": {
+      "id": 747,
+      "name": "Valentina",
+      "lastName": "Restrepo"
+  },
+  "7180": {
+      "id": 7180,
+      "name": "Angel",
+      "lastName": "Góez Giraldo"
+  }
+}
+
+server.get('/api', (req: Request, res: Response) => {
+  res.send(users)
+})
+
+server.get('/api/:id', (req: Request, res: Response) => {
+  res.send(users[req.params.id])
+})
+
+server.listen(8080, () => {
+  console.log('App is up and running', 80)
+})
